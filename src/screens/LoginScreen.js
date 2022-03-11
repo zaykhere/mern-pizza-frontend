2fe352f6c8933e00311af4bbf131ffe3e0bf98cb
Zain Javed <zainjhere@gmail.com>
2022-03-11 18:57:58 +0500

    Login Screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function LoginScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');  
+
+  function login() {
+    if(email==='' || password ==='') {
+        alert("All fields are required");
+    }
+    else {
+        const user = {email, password};
+        console.log(user);
+    }
+  }
+
   return (
-    <div>LoginScreen</div>
-  )
+    <div className="mt-3">
+      <div className="row justify-content-center">
+        <div className="col-md-5">
+          <h2 className="text-center"> Login your account! </h2>
+          <div className="mt-4">
+            <input
+              type="email"
+              placeholder="Enter email..."
+              className="form-control"
+              value={email}
+              required
+              onChange={(e)=> {setEmail(e.target.value)}}
+            />
+            <input
+              type="password"
+              placeholder="Enter password.."
+              className="form-control"
+              value={password}
+              required
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <button onClick={login} className="btn mt-3">LOGIN</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default LoginScreen
+export default LoginScreen;
