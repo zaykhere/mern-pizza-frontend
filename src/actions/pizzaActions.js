c8637d5d1798caf511636de73f18f9fdc31caf76
Zain Javed <zainjhere@gmail.com>
2022-03-20 05:32:29 +0500

    filter pizzas

--- src/actions/pizzaActions.js
+++ src/actions/pizzaActions.js
@@ -6,15 +6,36 @@ import {
 } from "../constants/pizzaConstants";
 
 export const getAllPizzas = () => async (dispatch) => {
-  dispatch({type: GET_PIZZAS_REQUEST})  
+  dispatch({ type: GET_PIZZAS_REQUEST });
   try {
     const response = await axios.get("/api/pizza");
 
-    dispatch({type: GET_PIZZAS_SUCCESS, payload: response.data})
-    
+    dispatch({ type: GET_PIZZAS_SUCCESS, payload: response.data });
   } catch (error) {
-
     console.log(error);
-    dispatch({ type: GET_PIZZAS_FAILED, payload: error.message});
+    dispatch({ type: GET_PIZZAS_FAILED, payload: error.message });
+  }
+};
+
+export const filterPizzas = (searchKey, category) => async (dispatch) => {
+  let filteredPizzas;
+  searchKey = searchKey.toLowerCase();
+  dispatch({ type: GET_PIZZAS_REQUEST });
+  try {
+    const response = await axios.get("/api/pizza");
+    filteredPizzas = response.data.filter((pizza) =>
+      pizza.name.toLowerCase().includes(searchKey)
+    );
+
+    if(category!=="all") {
+      filteredPizzas = response.data.filter((pizza) =>
+      pizza.category.toLowerCase()==category
+    );
+    }
+
+    dispatch({ type: GET_PIZZAS_SUCCESS, payload: filteredPizzas });
+  } catch (error) {
+    console.log(error);
+    dispatch({ type: GET_PIZZAS_FAILED, payload: error.message });
   }
 };
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { filterPizzas } from "../actions/pizzaActions";
 
 function Filter() {
   const dispatch = useDispatch();
 
+  const [searchKey, setSearchKey] = useState("");
+  const [category, setCategory] = useState("all");
+
   return (
     <div className="container">
       <div className="row filter-row justify-content-center shadow-lg p-3 mb-5 bg-white rounded">
-        <div className="col-md-3" style={{fontSize: '16px !important'}}>
+        <div className="col-md-3" style={{ fontSize: "16px !important" }}>
           <input
             type="text"
             className="form-control search-pizza"
             placeholder="Search Pizzas"
+            value={searchKey}
+            onChange={(e)=> {setSearchKey(e.target.value)}}
           />
         </div>
         <div className="col-md-3 mt-2">
-            <select className="form-control">
-                <option value="all"> All </option>
-                <option value="veg"> Veg </option>
-                <option value="nonveg"> NonVeg </option>
-            </select>
+          <select className="form-control"
+          value={category} 
+          onChange={(e)=> {setCategory(e.target.value)}} >
+            <option value="all"> All </option>
+            <option value="veg"> Veg </option>
+            <option value="nonveg"> NonVeg </option>
+          </select>
         </div>
         <div className="col-md-3 mt-2">
-            <button className="btn"> Filter</button>
+          <button
+            className="btn"
+            onClick={() => {
+              dispatch(filterPizzas(searchKey, category));
+            }}
+          >
+            Filter
+          </button>
         </div>
       </div>
     </div>
