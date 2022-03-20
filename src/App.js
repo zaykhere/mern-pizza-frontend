118075fef16683f45ff1d51c0d026659824d51cb
Zain Javed <zainjhere@gmail.com>
2022-03-20 19:58:49 +0500

    Pizza list screen

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from "./components/Navbar";
 import Homescreen from "./screens/Homescreen";
-import {BrowserRouter, Link, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import CartScreen from "./screens/CartScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import LoginScreen from "./screens/LoginScreen";
@@ -8,10 +8,6 @@ import OrdersScreen from "./screens/OrdersScreen";
 import ProtectedRoute from "./components/ProtectedRoute";
 import AdminScreen from "./screens/AdminScreen";
 import AdminRoute from "./components/AdminRoute";
-import UsersList from "./screens/UsersList";
-import PizzasList from "./screens/PizzasList";
-import OrdersList from "./screens/OrdersList";
-import AddPizza from "./screens/AddPizza";
 
 function App() {
   return (
--- src/screens/PizzasList.js
+++ src/screens/PizzasList.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllPizzas } from "../actions/pizzaActions";
+import Error from "../components/Error";
+import Loading from "../components/Loading";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 function PizzasList() {
+  const dispatch = useDispatch();
+
+  const pizzasState = useSelector((state) => state.getAllPizzasReducer);
+  const { pizzas, error, loading } = pizzasState;
+
+  useEffect(() => {
+    dispatch(getAllPizzas());
+  }, []);
+
   return (
-    <div>PizzasList</div>
+    <div>
+      <h2 style={{fontSize:"28px"}} className="mt-2 mb-2 text-center">
+        Pizzas List
+      </h2>
+      {loading && (<Loading />)}
+      {error && (<Error error={error} />)}
+      <table className="table table-bordered">
+        <thead className="table-dark">
+          <tr> 
+            <th>Name</th>
+            <th>Prices</th>
+            <th>Category</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+      {pizzas && pizzas.map((pizza)=>(
+        <tr> 
+          <td> {pizza.name} </td>
+          <td> 
+            Small: {pizza.prices[0]['small']}  <br /> 
+            Medium: {pizza.prices[0]['medium']} <br />
+            Large: {pizza.prices[0]['large']}  
+          </td>
+          <td> {pizza.category} </td>
+          <td>  <FontAwesomeIcon
+                  icon={faTrash}
+                  className="icon m-2"
+                  color="red"
+                /> 
+                
+                <FontAwesomeIcon
+                  icon={faEdit}
+                  className="icon m-2"
+                  color="black"
+                />
+                
+                </td>
+        </tr>
+      ))}
+      </tbody>
+     </table> 
+    </div>
   )
 }
 
-export default PizzasList
+export default PizzasList 
